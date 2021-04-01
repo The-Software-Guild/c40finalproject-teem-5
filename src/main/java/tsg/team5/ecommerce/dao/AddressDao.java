dc20a956bc5fd9bfa61ebbc7e9111fb012c1d809
Justin Donayre <38052798+captainjus@users.noreply.github.com>
2021-04-01 14:46:11 -0500

    Some AddressDao

--- src/main/java/tsg/team5/ecommerce/dao/AddressDao.java
+++ src/main/java/tsg/team5/ecommerce/dao/AddressDao.java
@@ -1,4 +1,11 @@
 package tsg.team5.ecommerce.dao;
 
+import tsg.team5.ecommerce.entity.Address;
+
+import java.util.List;
+
 public interface AddressDao {
+    public Address getAddressById(int addressId);
+    public List<Address> getAllAddresses();
+    public
 }
