72a802d2604fba7d54294beb4efdba61c3a7f83a
raheleh zarei <raheleh.zarei@gmail.com>
2021-04-01 11:55:22 -0700

    PurchaseDao-done

--- src/main/java/tsg/team5/ecommerce/dao/PurchaseDao.java
+++ src/main/java/tsg/team5/ecommerce/dao/PurchaseDao.java
@@ -1,4 +1,18 @@
 package tsg.team5.ecommerce.dao;
 
+import tsg.team5.ecommerce.entity.Purchase;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface PurchaseDao {
+    Purchase getPurchaseById(int purchaseId);   //return a purchase object by id
+    List<Purchase> getAllPurchases();   //return list of all purchases
+    List<Purchase> getPurchaseRangeDate(LocalDate from, LocalDate to);  //return all purchases in date range
+    List<Purchase> getPurchaseForCustomer(int customerId);  //return all the purchases made by a particular customer
+    List<Purchase> getPurchaseByDate(LocalDate date);  //return all purchases made in a specific date
+    List<Purchase> getPurchasesByCurrency(String  curr);    //return all the purchases made with a specific currency
+    Purchase addPurchase(Purchase purchase);    //add a new purchase
+    void UpdatePurchase(Purchase purchase);    //update a purchase
 }
