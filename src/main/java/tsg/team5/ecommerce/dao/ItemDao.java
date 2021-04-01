703ec105da70e2e9faf317e714e580abf88d7032
Louis Moccia <lmocciaiii@gmail.com>
2021-04-01 15:57:52 -0400

    Implementation of Item Dao

--- src/main/java/tsg/team5/ecommerce/dao/ItemDao.java
+++ src/main/java/tsg/team5/ecommerce/dao/ItemDao.java
@@ -1,4 +1,15 @@
 package tsg.team5.ecommerce.dao;
 
+import tsg.team5.ecommerce.entity.Item;
+
+import java.util.List;
+
 public interface ItemDao {
+
+    List<Item> getAllItems();
+    Item getItemById(int Id);
+    Item addItem(Item item);
+    void updateItem(Item item);
+    void deleteItem(int Id);
+
 }
