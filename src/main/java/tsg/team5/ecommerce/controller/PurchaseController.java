0e3ca745563a0be7b981c57a66618c36f52d6091
Louis Moccia <lmocciaiii@gmail.com>
2021-04-05 12:58:01 -0400

    purchase controller

--- src/main/java/tsg/team5/ecommerce/controller/PurchaseController.java
+++ src/main/java/tsg/team5/ecommerce/controller/PurchaseController.java
@@ -1,4 +1,25 @@
 package tsg.team5.ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import tsg.team5.ecommerce.dao.PurchaseDao;
+import tsg.team5.ecommerce.entity.Purchase;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("cart/")
 public class PurchaseController {
+
+    @Autowired
+    PurchaseDao purchaseDao;
+
+    @PostMapping("makePurchase")
+    public Purchase makePurchase(@RequestBody Purchase purchase){
+
+        System.out.println(purchase.getPurchaseId());
+        System.out.println("axios contact");
+
+        return null;
+    }
+
 }
