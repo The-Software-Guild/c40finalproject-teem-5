ee704ddfa2955d4cd1379d4592e304ede6348194
Justin Donayre <38052798+captainjus@users.noreply.github.com>
2021-04-05 15:37:56 -0500

    Update store_page.js

--- ecommerce-react/src/pages/store_page.js
+++ ecommerce-react/src/pages/store_page.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react"
 import '../styles/store_page.css'
-import { Row, Container, Card, CardColumns, Button } from 'react-bootstrap'
+import { Row, Container, Card, CardDeck, Button, CardColumns } from 'react-bootstrap'
 
-const ItemCard = ({ item, index }) => {
+const ItemCard = ({ item }) => {
     return (
-        <Card style={{ maxWidth: "200px", borderStyle: "outset", padding:"5px"}}>
-            <Card.Body>
-                <Card.Img src={item.image} style={{ height: "100px" }} />
-                <Card.Title style={{ fontSize: "10px" }}>{item.title}</Card.Title>
-                <Card.Text style={{ fontSize: "10px" }}>
-                    {item.price.toFixed(2)}
-                    <br />
-                    {item.description}
-                </Card.Text>
-                <Button>Add to Cart</Button>
+        <div>
+            <Card style={{ maxHeight:"400px", maxWidth: "250px", borderStyle: "outset", padding: "5px" }}>
+                <Card.Body>
+                    <Card.Title style={{ fontSize: "14px" }}>
+                        {item.title}
+                        <br />
+                        {item.price.toFixed(2)}
+                    </Card.Title>
+                    <Card.Img src={item.image} style={{ height: "100px" }} />
+                    <Card.Text style={{ maxHeight:"80px", fontSize: "14px", overflow:"scroll", overflowX:"hidden"}}>
+                        {item.description}
+                    </Card.Text>
+                    <Button>Add to Cart</Button>
                 </Card.Body>
-                <Card.Footer style={{ fontSize: "10px" }}>{item.category}</Card.Footer>
-        </Card>
+            </Card>
+        </div>
     );
 }
 
@@ -36,11 +39,14 @@ class StorePage extends Component {
     render() {
         let { items } = this.props;
         return (
-            <div id="store_page" className="Store-page" style={{ flexDirection: "row" }}>
-                <div> Storefront </div>
-                            {items.map((item, i) => {
-                                return <ItemCard item={item} key={i} />
-                            })}
+            <div>
+                <Container>
+                    <CardColumns>
+                        {items.map((item, i) => {
+                            return <ItemCard item={item} key={i} />
+                        })}
+                    </CardColumns>
+                </Container>
             </div>
         )
     }
