12bce2d2f04ce7d423312704ee99eb14940cd4e9
Dominic Tardif <64061564+GomuNingen@users.noreply.github.com>
2021-04-04 23:42:30 -0400

    25/25 tests passed, only controllers remain

--- src/test/java/tsg/team5/ecommerce/dao/PurchaseDaoDBTest.java
+++ src/test/java/tsg/team5/ecommerce/dao/PurchaseDaoDBTest.java
@@ -440,13 +440,323 @@ class PurchaseDaoDBTest {
 
     @Test
     void getPurchasesForCustomer() {
+        Item item = new Item();
+        item.setItemId(1);
+        item.setItemName("example1");
+        item.setCategory("category1");
+        item.setPrice(2.10);
+        item.setQuantity(5);
+        item = itemDao.addItem(item);
+
+        Item item2 = new Item();
+        item2.setItemId(2);
+        item2.setItemName("example2");
+        item2.setCategory("category2");
+        item2.setPrice(2.50);
+        item2.setQuantity(10);
+        item2 = itemDao.addItem(item2);
+
+        List<Item> items = new ArrayList<>();
+        items.add(item);
+        items.add(item2);
+
+        Exchange exchange = new Exchange();
+        exchange.setCny(new BigDecimal("1.1234"));
+        exchange.setCad(new BigDecimal("1.1234"));
+        exchange.setEur(new BigDecimal("1.1234"));
+        exchange.setGbp(new BigDecimal("1.1234"));
+        exchange.setJpy(new BigDecimal("1.1234"));
+        exchange = exchangeDao.addExchange(exchange);
+
+        Exchange exchange2 = new Exchange();
+        exchange2.setCny(new BigDecimal("1.2234"));
+        exchange2.setCad(new BigDecimal("1.2234"));
+        exchange2.setEur(new BigDecimal("1.2234"));
+        exchange2.setGbp(new BigDecimal("1.2234"));
+        exchange2.setJpy(new BigDecimal("1.2234"));
+        exchange2 = exchangeDao.addExchange(exchange2);
+
+        Exchange exchange3 = new Exchange();
+        exchange3.setCny(new BigDecimal("1.3234"));
+        exchange3.setCad(new BigDecimal("1.3234"));
+        exchange3.setEur(new BigDecimal("1.3234"));
+        exchange3.setGbp(new BigDecimal("1.3234"));
+        exchange3.setJpy(new BigDecimal("1.3234"));
+        exchange3 = exchangeDao.addExchange(exchange3);
+
+        Address address = new Address();
+        address.setStreet("Street 1");
+        address.setCity("City 1");
+        address.setCountry("Country 1");
+        address.setState("TX");
+        address.setPostal("85490");
+        address = addressDao.addAddress(address);
+
+        Address address2 = new Address();
+        address2.setStreet("Street 2");
+        address2.setCity("City 2");
+        address2.setCountry("Country 2");
+        address2.setState("CA");
+        address2.setPostal("90210");
+        address2 = addressDao.addAddress(address2);
+
+        Customer customer = new Customer();
+        customer.setCustomerName("Name 1");
+        customer.setCustomerEmail("abc@abc.com");
+        customer.setCustomerPhone("3879894356");
+        customer.setAddress(address);
+        customer = customerDao.addCustomer(customer);
+
+        Customer customer2 = new Customer();
+        customer2.setCustomerName("Name 2");
+        customer2.setCustomerEmail("xyz@abc.com");
+        customer2.setCustomerPhone("1112224466");
+        customer2.setAddress(address2);
+        customer2 = customerDao.addCustomer(customer2);
+
+        Purchase purchase = new Purchase();
+        purchase.setPurchaseDate(LocalDate.parse("2021-03-08"));
+        purchase.setCurrency("CAD");
+        purchase.setExchange(exchange);
+        purchase.setCustomer(customer);
+        purchase.setItems(items);
+        purchase = purchaseDao.addPurchase(purchase);
+
+        Purchase purchase2 = new Purchase();
+        purchase2.setPurchaseDate(LocalDate.parse("2021-03-21"));
+        purchase2.setCurrency("EUR");
+        purchase2.setExchange(exchange2);
+        purchase2.setCustomer(customer2);
+        purchase2.setItems(items);
+        purchase2 = purchaseDao.addPurchase(purchase2);
+
+        Purchase purchase3 = new Purchase();
+        purchase3.setPurchaseDate(LocalDate.parse("2021-04-01"));
+        purchase3.setCurrency("EUR");
+        purchase3.setExchange(exchange3);
+        purchase3.setCustomer(customer2);
+        purchase3.setItems(items);
+        purchase3 = purchaseDao.addPurchase(purchase3);
+
+        List<Purchase> fromDao = purchaseDao.getPurchasesForCustomer(customer2.getCustomerId());
+        assertEquals(2, fromDao.size());
+        assertFalse(fromDao.contains(purchase));
+        assertTrue(fromDao.contains(purchase2));
+        assertTrue(fromDao.contains(purchase3));
     }
 
     @Test
     void getPurchasesByDate() {
+        Item item = new Item();
+        item.setItemId(1);
+        item.setItemName("example1");
+        item.setCategory("category1");
+        item.setPrice(2.10);
+        item.setQuantity(5);
+        item = itemDao.addItem(item);
+
+        Item item2 = new Item();
+        item2.setItemId(2);
+        item2.setItemName("example2");
+        item2.setCategory("category2");
+        item2.setPrice(2.50);
+        item2.setQuantity(10);
+        item2 = itemDao.addItem(item2);
+
+        List<Item> items = new ArrayList<>();
+        items.add(item);
+        items.add(item2);
+
+        Exchange exchange = new Exchange();
+        exchange.setCny(new BigDecimal("1.1234"));
+        exchange.setCad(new BigDecimal("1.1234"));
+        exchange.setEur(new BigDecimal("1.1234"));
+        exchange.setGbp(new BigDecimal("1.1234"));
+        exchange.setJpy(new BigDecimal("1.1234"));
+        exchange = exchangeDao.addExchange(exchange);
+
+        Exchange exchange2 = new Exchange();
+        exchange2.setCny(new BigDecimal("1.2234"));
+        exchange2.setCad(new BigDecimal("1.2234"));
+        exchange2.setEur(new BigDecimal("1.2234"));
+        exchange2.setGbp(new BigDecimal("1.2234"));
+        exchange2.setJpy(new BigDecimal("1.2234"));
+        exchange2 = exchangeDao.addExchange(exchange2);
+
+        Exchange exchange3 = new Exchange();
+        exchange3.setCny(new BigDecimal("1.3234"));
+        exchange3.setCad(new BigDecimal("1.3234"));
+        exchange3.setEur(new BigDecimal("1.3234"));
+        exchange3.setGbp(new BigDecimal("1.3234"));
+        exchange3.setJpy(new BigDecimal("1.3234"));
+        exchange3 = exchangeDao.addExchange(exchange3);
+
+        Address address = new Address();
+        address.setStreet("Street 1");
+        address.setCity("City 1");
+        address.setCountry("Country 1");
+        address.setState("TX");
+        address.setPostal("85490");
+        address = addressDao.addAddress(address);
+
+        Address address2 = new Address();
+        address2.setStreet("Street 2");
+        address2.setCity("City 2");
+        address2.setCountry("Country 2");
+        address2.setState("CA");
+        address2.setPostal("90210");
+        address2 = addressDao.addAddress(address2);
+
+        Customer customer = new Customer();
+        customer.setCustomerName("Name 1");
+        customer.setCustomerEmail("abc@abc.com");
+        customer.setCustomerPhone("3879894356");
+        customer.setAddress(address);
+        customer = customerDao.addCustomer(customer);
+
+        Customer customer2 = new Customer();
+        customer2.setCustomerName("Name 2");
+        customer2.setCustomerEmail("xyz@abc.com");
+        customer2.setCustomerPhone("1112224466");
+        customer2.setAddress(address2);
+        customer2 = customerDao.addCustomer(customer2);
+
+        Purchase purchase = new Purchase();
+        purchase.setPurchaseDate(LocalDate.parse("2021-03-08"));
+        purchase.setCurrency("CAD");
+        purchase.setExchange(exchange);
+        purchase.setCustomer(customer);
+        purchase.setItems(items);
+        purchase = purchaseDao.addPurchase(purchase);
+
+        Purchase purchase2 = new Purchase();
+        purchase2.setPurchaseDate(LocalDate.parse("2021-03-21"));
+        purchase2.setCurrency("EUR");
+        purchase2.setExchange(exchange2);
+        purchase2.setCustomer(customer2);
+        purchase2.setItems(items);
+        purchase2 = purchaseDao.addPurchase(purchase2);
+
+        Purchase purchase3 = new Purchase();
+        purchase3.setPurchaseDate(LocalDate.parse("2021-04-01"));
+        purchase3.setCurrency("EUR");
+        purchase3.setExchange(exchange3);
+        purchase3.setCustomer(customer2);
+        purchase3.setItems(items);
+        purchase3 = purchaseDao.addPurchase(purchase3);
+
+        LocalDate date = LocalDate.parse("2021-03-21");
+        List<Purchase> fromDao = purchaseDao.getPurchasesByDate(date);
+        assertEquals(1, fromDao.size());
+        assertFalse(fromDao.contains(purchase));
+        assertTrue(fromDao.contains(purchase2));
+        assertFalse(fromDao.contains(purchase3));
     }
 
     @Test
     void getPurchasesByCurrency() {
+        Item item = new Item();
+        item.setItemId(1);
+        item.setItemName("example1");
+        item.setCategory("category1");
+        item.setPrice(2.10);
+        item.setQuantity(5);
+        item = itemDao.addItem(item);
+
+        Item item2 = new Item();
+        item2.setItemId(2);
+        item2.setItemName("example2");
+        item2.setCategory("category2");
+        item2.setPrice(2.50);
+        item2.setQuantity(10);
+        item2 = itemDao.addItem(item2);
+
+        List<Item> items = new ArrayList<>();
+        items.add(item);
+        items.add(item2);
+
+        Exchange exchange = new Exchange();
+        exchange.setCny(new BigDecimal("1.1234"));
+        exchange.setCad(new BigDecimal("1.1234"));
+        exchange.setEur(new BigDecimal("1.1234"));
+        exchange.setGbp(new BigDecimal("1.1234"));
+        exchange.setJpy(new BigDecimal("1.1234"));
+        exchange = exchangeDao.addExchange(exchange);
+
+        Exchange exchange2 = new Exchange();
+        exchange2.setCny(new BigDecimal("1.2234"));
+        exchange2.setCad(new BigDecimal("1.2234"));
+        exchange2.setEur(new BigDecimal("1.2234"));
+        exchange2.setGbp(new BigDecimal("1.2234"));
+        exchange2.setJpy(new BigDecimal("1.2234"));
+        exchange2 = exchangeDao.addExchange(exchange2);
+
+        Exchange exchange3 = new Exchange();
+        exchange3.setCny(new BigDecimal("1.3234"));
+        exchange3.setCad(new BigDecimal("1.3234"));
+        exchange3.setEur(new BigDecimal("1.3234"));
+        exchange3.setGbp(new BigDecimal("1.3234"));
+        exchange3.setJpy(new BigDecimal("1.3234"));
+        exchange3 = exchangeDao.addExchange(exchange3);
+
+        Address address = new Address();
+        address.setStreet("Street 1");
+        address.setCity("City 1");
+        address.setCountry("Country 1");
+        address.setState("TX");
+        address.setPostal("85490");
+        address = addressDao.addAddress(address);
+
+        Address address2 = new Address();
+        address2.setStreet("Street 2");
+        address2.setCity("City 2");
+        address2.setCountry("Country 2");
+        address2.setState("CA");
+        address2.setPostal("90210");
+        address2 = addressDao.addAddress(address2);
+
+        Customer customer = new Customer();
+        customer.setCustomerName("Name 1");
+        customer.setCustomerEmail("abc@abc.com");
+        customer.setCustomerPhone("3879894356");
+        customer.setAddress(address);
+        customer = customerDao.addCustomer(customer);
+
+        Customer customer2 = new Customer();
+        customer2.setCustomerName("Name 2");
+        customer2.setCustomerEmail("xyz@abc.com");
+        customer2.setCustomerPhone("1112224466");
+        customer2.setAddress(address2);
+        customer2 = customerDao.addCustomer(customer2);
+
+        Purchase purchase = new Purchase();
+        purchase.setPurchaseDate(LocalDate.parse("2021-03-08"));
+        purchase.setCurrency("CAD");
+        purchase.setExchange(exchange);
+        purchase.setCustomer(customer);
+        purchase.setItems(items);
+        purchase = purchaseDao.addPurchase(purchase);
+
+        Purchase purchase2 = new Purchase();
+        purchase2.setPurchaseDate(LocalDate.parse("2021-03-21"));
+        purchase2.setCurrency("EUR");
+        purchase2.setExchange(exchange2);
+        purchase2.setCustomer(customer2);
+        purchase2.setItems(items);
+        purchase2 = purchaseDao.addPurchase(purchase2);
+
+        Purchase purchase3 = new Purchase();
+        purchase3.setPurchaseDate(LocalDate.parse("2021-04-01"));
+        purchase3.setCurrency("EUR");
+        purchase3.setExchange(exchange3);
+        purchase3.setCustomer(customer2);
+        purchase3.setItems(items);
+        purchase3 = purchaseDao.addPurchase(purchase3);
+
+        List<Purchase> fromDao = purchaseDao.getPurchasesByCurrency("EUR");
+        assertEquals(2, fromDao.size());
+        assertFalse(fromDao.contains(purchase));
+        assertTrue(fromDao.contains(purchase2));
+        assertTrue(fromDao.contains(purchase3));
     }
 }
