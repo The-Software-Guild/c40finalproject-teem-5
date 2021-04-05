aa17be03cad4b525d43ace828a2306a20d395ef6
louismoccia <79379524+louismoccia@users.noreply.github.com>
2021-04-05 15:44:08 -0400

    Add files via upload

--- ecommerce-react/src/App.js
+++ ecommerce-react/src/App.js
@@ -5,48 +5,49 @@ import NavBar from "./components/nav_bar"
 import CheckoutPage from "./pages/checkout_page";
 import StorePage from './pages/store_page'
 import DataPage from './pages/data_page'
-
-const STORE_URL = "https://fakestoreapi.com"
-const EXCHANGE_RATE_URL = "http://data.fixer.io/api"
+import axios from 'axios';
+import UserServiceFetch from './services/UserServiceFetch'
 
 class App extends Component {
     state = {
         userId: null,
-        itemData: [{
-            id: null,
-            title: "test product",
-            price: 1.99,
-            description: "lorem ipsum set",
-            image: "https://i.pravatar.cc",
-            category: "electronic"
-        }],
-        cartData: [{
-            title: "test product",
-            price: 1.99,
-            exchangeRate: {
-                USD: 1.1234,
-                CAD: 1.1234,
-                EUR: 1.1234,
-                GBP: 1.1234,
-                JPY: 1.1234,
-            },
-            quantity: 0
-        }]
+        itemData: [
+            {
+                title: "test product",
+                price: 1.99,
+                description: "lorem ipsum set",
+                image: "https://i.pravatar.cc",
+                category: "electronic"
+            }
+        ],
+        cartData: [
+            {
+                title: "test product",
+                price: 1.99,
+                exchangeRate: {
+                    USD: 1.1234,
+                    CAD: 1.1234,
+                    EUR: 1.1234,
+                    GBP: 1.1234,
+                    JPY: 1.1234,
+                },
+                quantity: 0
+            }
+        ],
+        currentCurrency:"USD"
     }
 
-    componentDidMount() {
-        console.log("App is now mounted.")
-        this.loadItemData();
+    handleCurrencySelect = (event) =>{
+        let selected = event.target.value;
+        this.setState({currentCurrency:selected})
     }
 
-    loadItemData() {
-        this.setState({ loading: true })
-        console.log("Loading item data")
-        fetch(STORE_URL + "/products")
-            .then(data => data.json())
-            .then(data => this.setState(
-                { itemData: data, loading: false }
-            ))
+    handleTestAxios = (event) =>{
+
+        UserServiceFetch.addPurchase().then((response =>
+            console.log(response)
+        ));
+
     }
 
     render() {
@@ -55,12 +56,12 @@ class App extends Component {
                 <NavBar />
                 <main>
                     <Switch>
-                        <Route path='/store' render={props =>
-                            (<StorePage items={this.state.itemData} />)}
-                        />
+                        <Route exact path='/' component={StorePage} />
+                        <Route path='/store' component={StorePage} />
                         <Route path='/checkout' render={props =>
-                            (<CheckoutPage items={this.state.itemData} />)} />
-                        <Route path='/data' component={DataPage} />
+                        (<CheckoutPage items={this.state.cartData} currency={this.state.currentCurrency} handleCurrencySelect={this.handleCurrencySelect} handleTestAxios={this.handleTestAxios}/>)}/>
+
+                        <Route path='/data' component={DataPage}/>
                     </Switch>
                 </main>
             </div>
--- ecommerce-react/src/pages/store_page.js
+++ ecommerce-react/src/pages/store_page.js
@@ -1,51 +1,11 @@
-import React, { Component } from "react"
-import { Row, Container, Card, CardColumns } from 'react-bootstrap';
-
-const ItemCard = ({ item, index }) => {
+import React from "react"
+ 
+function StorePage() {
     return (
-        <Card bg="primary" text="white" style={{ minHeight: "200px", minWidth: "500px" , position: "relative"}}>
-            <Card.Body>
-                <Card.Img src={item.image} style={{ height: "100px" }} />
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Text>
-                    {item.price}
-                    <br />
-                    {item.description}
-                </Card.Text>
-                <Card.Footer>{item.category}</Card.Footer>
-            </Card.Body>
-        </Card>
-    );
+        <div id="home_page" className="App-page App-header">
+            stoire sotoer
+        </div>
+    )
 }
-
-class StorePage extends Component {
-
-    static defaultsProps = {
-        items: [{
-            id: 0,
-            title: "test product",
-            price: 1.99,
-            description: "lorem ipsum set",
-            image: "https://i.pravatar.cc",
-            category: "electronic"
-        }]
-    }
-
-    render() {
-        let { items } = this.props;
-        return (
-            <div id="store_page" className="App-page"
-            >
-                <Container>
-                        <CardColumns>
-                            {items.map((item, i) => {
-                                return <ItemCard item={item} key={i} />
-                            })}
-                        </CardColumns>
-                </Container>
-            </div>
-        )
-    }
-}
-
+ 
 export default StorePage
