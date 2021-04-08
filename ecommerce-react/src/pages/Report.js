b6e1b86c488de63239b95e05d4ec1aff7ac29c52
raheleh zarei <raheleh.zarei@gmail.com>
2021-04-08 14:50:54 -0700

    Report date range-in progress

--- ecommerce-react/src/pages/Report.js
+++ ecommerce-react/src/pages/Report.js
@@ -1,18 +1,36 @@
 import React, {Component} from "react";
-import {Button, Col, Table} from 'react-bootstrap';
+import {Row, Col, Table} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Report extends Component{
 
+     min = Date.parse('2001-10-01');
+     max = Date.parse('2022-01-01');
+        fromDate='';
+        toDate='';
+
+     //set the selected date
+    DateHandlerFrom = (f) => {
+        this.fromDate=f;
+    };
+
+    DateHandlerTo = (t) => {
+        this.toDate=t;
+    };
+
+
+
     render() {
+
         let {purchaseHistory,currency,totalCostOfPurchases , handleCurrencySelect} = this.props
         return (
             <div >
-                <Col >
+                <Row className="justify-content-md-center">
+                <Col  className="justify-content-md-center  border border-dark">
                     <h1 style={{color:"#069"}}> Purchases based on a currency</h1>
-                    <hr/>
+                    <Row className="justify-content-md-center">
                     <label htmlFor="date" className="col-3 col-form-label">
-                        From: </label>
+                        Currency: </label>
                     <select value={currency} onChange={handleCurrencySelect}>
                         <option selected value="USD">USD</option>
                         <option value="CAD">CAD</option>
@@ -20,6 +38,7 @@ class Report extends Component{
                         <option value="GBP">GBP</option>
                         <option value="JPY">JPY</option>
                     </select>
+                    </Row>
                     <Table className="table table-striped">
                         <thead>
                         <tr>
@@ -46,41 +65,53 @@ class Report extends Component{
                         </tbody>
                     </Table>
                 </Col>
-                <Col >
+                <Col className="justify-content-md-center  border border-dark">
                     <h1 style={{color:"#069"}}> Purchases in range date</h1>
-                    <label htmlFor="date" className="col-3 col-form-label">
-                        From: </label>
-                    <div className="col-9">
-                        <input type="date" name="date"
-                               id="date" className="date-picker"/>
-                    </div>
-                    <label htmlFor="date" className="col-3 col-form-label">
-                        To:</label>
-                    <div className="col-9">
-                        <input type="date" name="date"
-                               id="date" className="date-picker"/>
-                    </div>
+                    <Row className="justify-content-md-center">
+                        <Col>
+                            <label htmlFor="date" >
+                                From: </label>
+                            <div >
+                                <input type="date" name="date"
+                                       id="from" className="date-picker" onChange={(e)=> this.DateHandlerFrom(e.target.value)}/>
+                            </div>
+                        </Col>
+                        <Col>
+                            <label htmlFor="date" >
+                                To:</label>
+                            <div>
+                                <input type="date" name="date"
+                                       id="to" className="date-picker" onChange={(e)=> this.DateHandlerTo(e.target.value)}/>
+                            </div>
+                        </Col>
+                    </Row>
                     <Table className="table table-striped">
-                <thead>
-                    <tr>
-                        <td>purchaseDate</td>
-                        <td>total Cost</td>
-                        <td>currency</td>
-                        <td>customerName</td>
-                        <td>country</td>
-                        <td>Purchase Detail</td>
-                    </tr>
-                </thead>
+                        <thead>
+                            <tr>
+                                <td>purchaseDate</td>
+                                <td>total Cost</td>
+                                <td>currency</td>
+                                <td>customerName</td>
+                                <td>country</td>
+                            </tr>
+                        </thead>
                         <tbody>
-                        <tr>
-                    {purchaseHistory.filter(history => history.purchaseDate.includes('J'))
-                        .map(filteredName => (<td></td>))
-
-                    }
-                        </tr>
+                        {
+                            purchaseHistory.filter(history => (history.purchaseDate>this.fromDate) && (history.purchaseDate<this.toDate))
+                                .map(filteredName => (
+                                    <tr key={filteredName.purchaseId}>
+                                        <td>{filteredName.purchaseDate }</td>
+                                        <td>{totalCostOfPurchases[filteredName.purchaseId]}</td>
+                                        <td>{filteredName.currency}</td>
+                                        <td>{filteredName.customer.customerName}</td>
+                                        <td>{filteredName.customer.address.country}</td>
+                                    </tr>
+                                ))
+                        }
                         </tbody>
                     </Table>
                 </Col>
+                </Row>
             </div>
         );
     }
