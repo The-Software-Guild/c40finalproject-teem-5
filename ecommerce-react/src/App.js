9874ade8e9f3431201b2fde4c67301f79214d7ed
Justin Donayre <38052798+captainjus@users.noreply.github.com>
2021-04-09 13:46:22 -0500

    Update App.js

--- ecommerce-react/src/App.js
+++ ecommerce-react/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom"
 import './App.css'
-
 import NavBar from "./components/nav_bar"
 import CheckoutPage from "./pages/checkout_page";
 import StorePage from './pages/store_page'
+import DataPage from './pages/data_page'
 import LoginPage from './pages/login_page'
-import PurchaseHistory from "./pages/PurchaseHistory";
 import axios from 'axios';
-import Report from "./pages/Report";
+
+import UserServiceFetch from './services/UserServiceFetch'
 
 const STORE_URL = "https://fakestoreapi.com"
 const EXCHANGE_RATE_URL = "https://api.ratesapi.io/api"
@@ -42,28 +42,49 @@ class App extends Component {
             JPY: 1.1234,
             CNY: 1.1234
         },
-        LiveExchangeRate:{
-            base: 'USD',
-            rates: {
-                EUR: '',
-                CAD: '',
-                JPY: '',
-                CNY: '',
-                GBP: ''
-            },
-             date: ''
-             },
         currentCurrency: "USD",
-        customerId: 1,
+        customerId: 0,
         addressId: 0,
         totalCost: "0.00",
+        customerInfo: {
+            //customer
+                name: "",   // max 50 char
+                email: "",  // max 50 char
+                phone: "",   // max 10 char and needs regex validation (?)
+            //address
+                street: "", // max 30 char
+                city: "",   // max 30 char
+                state: "",  // max 2 char (should be handled by <select> input method)
+                postal: "", // max 10 char
+                country: "" // max 30 char
+        }
     }
 
-    constructor(props){
-        super(props);
-        this.handleClearItems = this.handleClearItems.bind(this);
+    handleCustomerCreation = (event, newName, newEmail, newPhone,
+        newStreet, newCity, newState, newPostal, newCountry) => {
+        event.preventDefault();
+
+        var newCustomer = {
+            name: newName,
+            email: newEmail,
+            phone: newPhone
+        }
+        var newAddress = {
+            street: newStreet,
+            city: newCity,
+            state: newState,
+            postal: newPostal,
+            country: newCountry
+        }
+        this.setState({
+            customer: newCustomer,
+            address: newAddress
+        })
+        alert("Customer created!")
+        console.log(this.state.CustomerInfo.customer)
+        console.log(this.state.CustomerInfo.address)
     }
-    
+
     handleCurrencySelect = (event) => {
         let selected = event.target.value;
         this.setState({ currentCurrency: selected })
@@ -76,6 +97,7 @@ class App extends Component {
         this.setState({ customerId: selected })
         console.log(selected)
     }
+
 
     handleAddressSelect = (event) => {
         let selected = event.target.value;
@@ -99,7 +121,7 @@ class App extends Component {
     handleTotalCalculation = async() =>{
         console.log("test");
 
-        let USDRates = await axios.get(EXCHANGE_RATE_URL + '/latest?base=USD&symbols=CAD,EUR,GBP,JPY,CNY')
+        let USDRates = await axios.get('https://api.ratesapi.io/api/latest?base=USD&symbols=CAD,EUR,GBP,JPY,CNY')
                     .then(response => response.data.rates);
 
        this.setState({exchangeRate:USDRates});
@@ -109,13 +131,13 @@ class App extends Component {
            cart:this.state.cartData,
            currency:this.state.currentCurrency,
            exchange:this.state.exchangeRate
-       }).then(response =>
+       }).then(response => 
                 this.setState({cartData:response.data}));
     }
 
     handleTestAxios = async (event) => {
 
-        let USDRates = await axios.get(EXCHANGE_RATE_URL + '/latest?base=USD&symbols=CAD,EUR,GBP,JPY,CNY')
+        let USDRates = await axios.get('https://api.ratesapi.io/api/latest?base=USD&symbols=CAD,EUR,GBP,JPY,CNY')
             .then(response => response.data.rates)
 
         this.setState({ exchangeRate: USDRates });
@@ -131,52 +153,19 @@ class App extends Component {
             }).then(response =>
                 this.setState({ totalCost: parseFloat(response.data.totalCost).toFixed(2) })
             );
-
-        alert("Cart Purchased");
     }
 
-    handleClearItems(){
-        const reset = 
-        {
-            title: null,
-            price: null,
-            quantity: null,
-            itemId: null,
-            category: null,
-            totalForCard: null
-        };
-        this.setState({ cartData:reset });
-        
-    }
-    //mount all the data
     componentDidMount() {
         console.log("App is now mounted.")
         this.loadItemData();
-        this.loadPurchaseHistory();
-        this.loadExchangeRate();
-        this.loadPurchaseTotalCost();
         this.state.cartData.pop();
+<<<<<<< HEAD
+        
+=======
+        console.log(this.state);
+>>>>>>> Justin-Branch
+    }
 
-    }
-    //retrieve current exchange rate
-    loadExchangeRate(){
-        axios.get(EXCHANGE_RATE_URL + '/latest?base=USD&symbols=CAD,EUR,GBP,JPY,CNY').then(
-            ((res) =>this.setState({LiveExchangeRate:res.data})
-        ))
-    }
-    //retrieve purchase history from database
-    loadPurchaseHistory()
-    {
-        axios.get('http://localhost:8080/cart/history').then((res)=>
-            this.setState({purchaseHistory:res.data}))
-    }
-    //retrieve total cost for each purchase from service layer
-    loadPurchaseTotalCost()
-    {
-        axios.get('http://localhost:8080/cart/totals').then((res)=>
-            this.setState({totalCostOfPurchases:res.data}))
-    }
-    //set items, current exchange rates and purchase history
     loadItemData() {
         this.setState({ loading: true })
         console.log("Loading item data")
@@ -187,6 +176,7 @@ class App extends Component {
             ))
     }
 
+
     render() {
         return (
             <div className="App">
@@ -194,8 +184,9 @@ class App extends Component {
                 <main>
                     <Switch>
                         <Route exact path='/' render={props =>
-                        (<LoginPage customer={this.state.customerId} address={this.state.addressId}
-                            selectCustomer={this.handleCustomerSelect} selectAddress={this.handleAddressSelect} />)}
+                        (<LoginPage customer={this.state.customerId} selectCustomer={this.handleCustomerSelect}
+                            handleSubmit={this.handleCustomerCreation}
+                        />)}
                         />
                         <Route path='/store' render={props =>
                         (<StorePage items={this.state.itemData} handleSelect={this.selectHandler}
@@ -204,19 +195,10 @@ class App extends Component {
                         <Route path='/checkout' render={props =>
                         (<CheckoutPage items={this.state.cartData} currency={this.state.currentCurrency}
                             handleCurrencySelect={this.handleCurrencySelect} handleTestAxios={this.handleTestAxios}
-                            totalCost={this.state.totalCost} handleTotalCalculation={this.handleTotalCalculation}
-                            exchangeRate = {this.state.LiveExchangeRate} handleClearItems={this.handleClearItems}/>)}
+                            totalCost={this.state.totalCost} handleTotalCalculation={this.handleTotalCalculation}/>)}
+                        />
 
-                        />
-                        <Route path='/history' render={props =>(<PurchaseHistory
-                               // purchaseHistory={this.state.purchaseHistory}
-                               // totalCostOfPurchases = {this.state.totalCostOfPurchases}
-                               />)}
-                        />
-                        <Route path='/report' render ={props =>
-                            <Report currency ={this.state.currentCurrency}
-                                    handleCurrencySelect ={this.handleCurrencySelect}
-                            />}/>
+                        <Route path='/data' component={DataPage} />
                     </Switch>
                 </main>
             </div>
